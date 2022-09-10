580fdb872846416dfc93aa71a3ad29c0b42e5c31
betasewer <gorosakata@ya.ru>
2022-09-11 00:34:02 +0900

    meta

--- machaon/types/meta.py
+++ machaon/types/meta.py
@@ -1,25 +1,69 @@
+"""
+型のtrait実装を直接操作する
+"""
 from machaon.core.importer import attribute_loader
-from machaon.core.type.typedef import TypeDefinition, ClassDescriber
+from machaon.types.numeric import Oct
 
 
-def _load_describer(describer):
-    if isinstance(describer, str):
-        return attribute_loader(describer)()
-    else:
-        return describer
+class Meta:
+    def __init__(self, describer, *, valueclass=False):
+        self._describer = describer
+        self._trait = not valueclass
+    
+    def constructor(self, *args):
+        """ この型のオブジェクトを作成する """
+        return self._describer.constructor(self._describer, *args)
+
+    def stringify(self, value):
+        """ この型のオブジェクトを引数にとり、文字列として返す """
+        if self._trait:
+            return self._describer.stringify(self._describer, value)
+        else:
+            return self._describer.stringify(value)
+    
+    def pprint(self, app, value):
+        """ この型のオブジェクトを引数にとり、文字列として返す """
+        if self._trait:
+            return self._describer.pprint(self._describer, value, app)
+        else:
+            return self._describer.pprint(value, app)
+    
+    def convert_stringify(self, *args):
+        """ この型のオブジェクトを作成し、そのまま文字列にして返す """
+        o = self.construct(*args)
+        return self.stringify(o)
+    
+
+class DefinedMeta:
+    def get(self, describer, **kwargs):
+        if isinstance(describer, str):
+            d = attribute_loader(describer)()
+        else:
+            d = describer
+        return Meta(d, **kwargs)
+
+    def __call__(self, describer, **kwargs):
+        return self.get(describer, **kwargs)
+
+    #
+    # ショートカット
+    #
+    def _shortcut(dest, **kwargs):
+        def getter(self):
+            return self.get(dest, **kwargs)
+        return property(fget=getter)
+
+    # numeric
+    LocaleInt       = _shortcut("machaon.types.numeric.LocaleInt")
+    LocaleFloat     = _shortcut("machaon.types.numeric.LocaleFloat")
+    # dateandtime
+    DateSeparated   = _shortcut("machaon.types.dateandtime.DateSeparated")
+    Date8           = _shortcut("machaon.types.dateandtime.Date8")
+    Date4           = _shortcut("machaon.types.dateandtime.Date4")
+    Month           = _shortcut("machaon.types.dateandtime.Month")
+    Day             = _shortcut("machaon.types.dateandtime.Day")
+    YearLowMonth    = _shortcut("machaon.types.dateandtime.YearLowMonth")
+    YearLowDate     = _shortcut("machaon.types.dateandtime.YearLowDate")
 
 
-def construct(type, *args):
-    """ コンストラクタを呼び出す """
-    t = _load_describer(type)
-    return t.constructor(t, *args)
-
-def stringify(type, *args):
-    """ 文字列化する """
-    t = _load_describer(type)
-    return t.stringify(t, *args)
-
-def pprint(type, app, *args):
-    """ 表示する """
-    t = _load_describer(type)
-    return t.pprint(t, app, *args)
+meta = DefinedMeta()
