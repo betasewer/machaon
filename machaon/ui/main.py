942aa93d165048e9a79f395001995136dcfce588
betasewer <gorosakata@ya.ru>
2022-10-14 03:37:51 +0900

    add --core-only option

--- machaon/ui/main.py
+++ machaon/ui/main.py
@@ -16,13 +16,14 @@ def initialize_app_args(root, argv=None, **defaults):
         prog = "machaon",
         description = "Machaon is a message-oriented Python application environment",
     )
+    pser.add_argument("-c", "--core-only", help="初期化時に外部パッケージ・ホットキーを読み込まない", action="store_const", const=True)
     pser.add_argument("-e", "--entrypoint", help="Pythonの関数を[モジュール名.変数名]の形式で指定して実行", action="append", default=[])
     pser.add_argument("-m", "--message", help="任意のメッセージを実行", action="append", default=[])
-    pser.add_argument("-d", "--deploy", help="machaonディレクトリを配備する")
     pser.add_argument("-u", "--ui", help="UIを指定する")
     pser.add_argument("-b", "--batch", help="バッチモードで起動する", action="store_const", dest="ui", const="batch")
-    pser.add_argument("-l", "--location", help="開始ディレクトリを指定する")
     pser.add_argument("-o", "--option", help="オプションを指定する: [NAME]=[VALUE]", action="append", default=[])
+    pser.add_argument("-d", "--dir", help="開始ディレクトリを指定する")
+    pser.add_argument("--deploy", help="machaonディレクトリを配備する")
     pser.add_argument("--update", help="全てのパッケージとmachaon本体をアップデートして終了する", action="store_const", const=True)
     pser.add_argument("--title", help="アプリの名前")
     args = pser.parse_args(argv)
@@ -30,7 +31,12 @@ def initialize_app_args(root, argv=None, **defaults):
     autoexit = False
 
     if args.update or args.deploy:
-        args.ui = defaults.get("ui", "batch")
+        if not args.ui:
+            args.ui = "batch"
+
+    if args.core_only:
+        root.ignore_at_startup("packages")
+        root.ignore_at_startup("hotkey")
 
     if args.entrypoint:
         for i, ent in enumerate(args.entrypoint, start=1):
@@ -44,14 +50,15 @@ def initialize_app_args(root, argv=None, **defaults):
         root.add_startup_variable("path", args.deploy, "Path")
         root.add_startup_message("@@deploy @path")
         autoexit = True
-        
+
     if args.update:
-        root.ignore_at_startup("packages")
         root.add_startup_message("@@update-all")
         root.add_startup_message("@@machaon-update")
         autoexit = True
 
     if autoexit:
+        root.ignore_at_startup("packages")
+        root.ignore_at_startup("hotkey")
         root.add_startup_message("exit")
     
     options = {}
@@ -66,8 +73,8 @@ def initialize_app_args(root, argv=None, **defaults):
     initargs.update(defaults)
     if args.ui:
         initargs["ui"] = args.ui
-    if args.location:
-        initargs["basic_dir"] = args.location
+    if args.dir:
+        initargs["basic_dir"] = args.dir
     if args.title:
         initargs["title"] = args.title
     initargs.update(options)
